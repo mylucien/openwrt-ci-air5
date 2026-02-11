0579afed6c7c12ffa489ab59d851e437be0afa7d
mylucien <53183558+mylucien@users.noreply.github.com>
2026-02-11 22:45:20 +0800

    Update softbank-air5.mk

--- air5/softbank-air5.mk
+++ air5/softbank-air5.mk
@@ -13,12 +13,7 @@ define Device/softbank_air5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_SIZE := 12288k
-  DEVICE_PACKAGES := ath11k-firmware-ipq8074 kmod-ath11k-ahb kmod-ath11k-pci \
-		kmod-usb3 kmod-usb-dwc3 kmod-usb-dwc3-qcom \
-		kmod-qca-nss-dp kmod-qca-ssdk qca-ssdk-shell \
-		kmod-usb-net-qmi-wwan kmod-usb-serial-option kmod-usb-serial-qualcomm \
-		uqmi umbim comgt comgt-ncm \
-		kmod-usb-net-cdc-mbim kmod-usb-net-cdc-ncm
+  DEVICE_PACKAGES := ath11k-firmware-ipq8074 kmod-ath11k-ahb kmod-ath11k-pci
   KERNEL := kernel-bin | gzip | fit gzip $$(KDIR)/image-$$(DEVICE_DTS).dtb
   IMAGES := sysupgrade.bin factory.bin
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | append-metadata
