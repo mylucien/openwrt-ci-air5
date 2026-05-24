e526aeac9851ceadbbd6397f619d3a48cb2c5a01
mylucien <53183558+mylucien@users.noreply.github.com>
2026-05-24 22:56:56 +0800

    Update softbank-air5.mk

--- air5/softbank-air5.mk
+++ air5/softbank-air5.mk
@@ -11,12 +11,15 @@ define Device/softbank_air5
   DEVICE_DTS := ipq8072-softbank-air5
   DEVICE_DTS_CONFIG := config@hk09
   KERNEL_SIZE := 8192k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
   DEVICE_PACKAGES := kmod-usb3 kmod-usb-dwc3 kmod-usb-dwc3-qcom \
   		kmod-qca-nss-dp kmod-qca-ssdk qca-ssdk-shell \
   		kmod-usb-net-qmi-wwan kmod-usb-serial-option kmod-usb-serial-qualcomm \
   		uqmi umbim comgt comgt-ncm \
   		kmod-usb-net-cdc-mbim kmod-usb-net-cdc-ncm
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb $$(DEVICE_DTS_CONFIG)
+  KERNEL_INITRAMFS := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb $$(DEVICE_DTS_CONFIG)
   IMAGES := sysupgrade.bin factory.bin
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | append-metadata
   IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | pad-rootfs
