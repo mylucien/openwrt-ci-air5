74feb2f1efefc4a2e7a01c2bcd0ffcc4aeebc2ef
mylucien <qq734348678@163.com>
2026-05-27 11:22:22 +0800

    fix: keep feeds golang (don't remove/replace it, avoid dependency errors)

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -9,10 +9,18 @@ sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "M
 
 # 移除要替换的包
 rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/lang/golang
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+
   branch="$1" repourl="$2" && shift 2
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
   repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
@@ -42,3 +50,5 @@ rm -rf feeds/packages/net/onionshare-cli
 # 处理gcc14 + fortify + Werror 的兼容性问题
 sed -i 's/-Werror=format-nonliteral/-Wno-error=format-nonliteral/g' package/libs/libubox/CMakeLists.txt 2>/dev/null || true
 sed -i 's/-Werror/-Wno-error/g' package/libs/libubox/CMakeLists.txt 2>/dev/null || true
+
+
